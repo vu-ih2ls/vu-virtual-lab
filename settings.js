07ceb1dcc43ce32d8f125346270af12029a50c85
Maro Donato <63956556+MaroDonato@users.noreply.github.com>
2024-03-02 11:03:56 +0100

    Added small screen warning popup

--- settings.js
+++ settings.js
@@ -2,7 +2,9 @@ const settings = {
   // Edit this url for different canvas urls or course codes.
   "baseUrl": "https://canvas.vu.nl/courses/<COURSE_CODE>/",
   // Name of the folder containing the files.
-  "folder": "img"
+  "folder": "img",
+  // Warning popup for small screens.
+  "smallScreenWarning": true
 };
 
 export default settings;
